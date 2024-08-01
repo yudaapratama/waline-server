4f4190fdae5a598284d3ebb85887db109ae917fb
yudaapratama <yudhaprathamaa@gmail.com>
2024-08-01 09:52:05 +0700

    fix: example server

--- config/extend.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('node-fetch');
-const Model = require('../../packages/server/node_modules/think-model');
-const Mongo = require('think-mongo');
-
-module.exports = [
-  Model(think.app)
-];
--- index.cjs
+++ index.cjs
@@ -1,14 +1,8 @@
 const Application = require('@waline/vercel');
-const CustomModel = require('./CustomModel.cjs');
-
 
 module.exports = Application({
-	env: 'development',
-	model: (tableName) => {
-		return this.service(`storage/mysql`, tableName)
-	},
   plugins: [],
-  async postSave(comment) {
-    // do what ever you want after comment saved
+  async preSave(comment) {
+		
   },
 });
